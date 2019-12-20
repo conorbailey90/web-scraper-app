be4402dd81bdb5cd6d04a527432c74f66a245389
conorbailey90 <conbailey90@gmail.com>
2019-12-20 14:52:56 +0000

    company screenshot router added

--- routes/companies.js
+++ routes/companies.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const puppeteer = require("puppeteer");
+// const express = require("express");
+// const puppeteer = require("puppeteer");
 
-const router = express.Router();
-const path = require("path");
+// const router = express.Router();
+// const path = require("path");
 
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "companyss.html"));
-});
+// router.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../public", "companyss.html"));
+// });
 
-module.exports = router;
+// module.exports = router;
---
+++ routes/companyscraper.js
@@ -0,0 +1,268 @@
+const puppeteer = require("puppeteer");
+const express = require("express");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  try {
+    let company = req.body.companyName;
+    console.log(company);
+    let website = "https://www.google.com";
+
+    let time = new Date();
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+
+    let dayOfAccess = `${time.getDate()} ${
+      monthNames[time.getMonth()]
+    } ${time.getFullYear()}`;
+    console.log(dayOfAccess);
+
+    const browser = await puppeteer.launch({
+      defaultViewport: { width: 1920, height: 2000 },
+      args: ["--no-sandbox"],
+      headless: false
+    });
+    const page = await browser.newPage();
+
+    await page.goto(website, { waitUntil: "networkidle2" });
+
+    let google = await page.evaluate(company => {
+      let input = document.querySelector(
+        "#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input"
+      );
+      input.value = `"${company}"`;
+
+      return;
+    }, company);
+
+    await page.evaluate(() => {
+      document
+        .querySelector(
+          "#tsf > div:nth-child(2) > div.A8SBwf > div.FPdoLc.tfB0Bf > center > input.gNO89b"
+        )
+        .click();
+    });
+    await page.waitForNavigation();
+
+    await page.evaluate(dayOfAccess => {
+      let dateDiv = document.createElement("div");
+      dateDiv.style.position = "absolute";
+      dateDiv.style.top = 0;
+      dateDiv.style.left = "50%";
+      dateDiv.style.width = "50%";
+      dateDiv.style.height = "5rem";
+      dateDiv.style.color = "white";
+      dateDiv.style.background = "black";
+      dateDiv.style.zIndex = 9999;
+      dateDiv.style.padding = "5px";
+      document.body.appendChild(dateDiv);
+      dateDiv.innerHTML = `<p>Date: ${dayOfAccess}</p>`;
+    }, dayOfAccess);
+
+    await page.screenshot({ path: "./public/companyss/google.png" });
+
+    let links = await page.evaluate(() => {
+      let pages = Array.from(document.querySelectorAll(".rc"));
+
+      let pageLinks = pages.map(page => {
+        return page.children[0].children[0].href;
+      });
+      return pageLinks;
+    });
+
+    await page.goto(links[0], { waitUntil: "networkidle2" });
+    await page.evaluate(dayOfAccess => {
+      let dateDiv = document.createElement("div");
+      dateDiv.style.position = "absolute";
+      dateDiv.style.top = 0;
+      dateDiv.style.left = "50%";
+      dateDiv.style.width = "50%";
+      dateDiv.style.height = "7rem";
+      dateDiv.style.color = "white";
+      dateDiv.style.background = "black";
+      dateDiv.style.zIndex = 9999;
+      dateDiv.style.padding = "5px";
+      document.body.appendChild(dateDiv);
+      dateDiv.innerHTML = `<p>Date: ${dayOfAccess}</p> <p>Address: ${location.href}</p>`;
+    }, dayOfAccess);
+
+    await page.screenshot({
+      path: "./public/companyss/link1.png"
+      // fullPage: true
+    });
+    await page.goto(links[1], { waitUntil: "networkidle2" });
+
+    await page.evaluate(dayOfAccess => {
+      let dateDiv = document.createElement("div");
+      dateDiv.style.position = "absolute";
+      dateDiv.style.top = 0;
+      dateDiv.style.left = "50%";
+      dateDiv.style.width = "50%";
+      dateDiv.style.height = "7rem";
+      dateDiv.style.color = "white";
+      dateDiv.style.background = "black";
+      dateDiv.style.zIndex = 9999;
+      dateDiv.style.padding = "5px";
+      document.body.appendChild(dateDiv);
+      dateDiv.innerHTML = `<p>Date: ${dayOfAccess}</p> <p>Address: ${location.href}</p>`;
+    }, dayOfAccess);
+
+    await page.screenshot({
+      path: "./public/companyss/link2.png"
+      // fullPage: true
+    });
+    await page.goto(links[2], { waitUntil: "networkidle2" });
+
+    await page.evaluate(dayOfAccess => {
+      let dateDiv = document.createElement("div");
+      dateDiv.style.position = "absolute";
+      dateDiv.style.top = 0;
+      dateDiv.style.left = "50%";
+      dateDiv.style.width = "50%";
+      dateDiv.style.height = "7rem";
+      dateDiv.style.color = "white";
+      dateDiv.style.background = "black";
+      dateDiv.style.zIndex = 9999;
+      dateDiv.style.padding = "5px";
+      document.body.appendChild(dateDiv);
+      dateDiv.innerHTML = `<p>Date: ${dayOfAccess}</p> <p>${location.href}</p>`;
+    }, dayOfAccess);
+
+    await page.screenshot({
+      path: "./public/companyss/link3.png"
+      // fullPage: true
+    });
+    await page.goto(links[3], { waitUntil: "networkidle2" });
+
+    await page.evaluate(dayOfAccess => {
+      let dateDiv = document.createElement("div");
+      dateDiv.style.position = "absolute";
+      dateDiv.style.top = 0;
+      dateDiv.style.left = "50%";
+      dateDiv.style.width = "50%";
+      dateDiv.style.height = "7rem";
+      dateDiv.style.color = "white";
+      dateDiv.style.background = "black";
+      dateDiv.style.zIndex = 9999;
+      dateDiv.style.padding = "5px";
+      document.body.appendChild(dateDiv);
+      dateDiv.innerHTML = `<p>Date: ${dayOfAccess}</p> <p>Address: ${location.href}</p>`;
+    }, dayOfAccess);
+
+    await page.screenshot({
+      path: "./public/companyss/link4.png"
+      // fullPage: true
+    });
+    await page.goto(links[4], { waitUntil: "networkidle2" });
+
+    await page.evaluate(dayOfAccess => {
+      let dateDiv = document.createElement("div");
+      dateDiv.style.position = "absolute";
+      dateDiv.style.top = 0;
+      dateDiv.style.left = "50%";
+      dateDiv.style.width = "50%";
+      dateDiv.style.height = "7rem";
+      dateDiv.style.color = "white";
+      dateDiv.style.background = "black";
+      dateDiv.style.zIndex = 9999;
+      dateDiv.style.padding = "5px";
+      document.body.appendChild(dateDiv);
+      dateDiv.innerHTML = `<p>Date: ${dayOfAccess}</p> <p>Address: ${location.href}</p>`;
+    }, dayOfAccess);
+
+    await page.screenshot({
+      path: "./public/companyss/link5.png"
+      // fullPage: true
+    });
+
+    await page.goto("https://www.dowjones.com/products/risk-compliance/", {
+      waitUntil: "networkidle2"
+    });
+
+    let loginPage = await page.evaluate(() => {
+      let link = document.querySelector(
+        "body > div.page-wrapper > header > div > div.header-right > div > div > div > ul > li:nth-child(5) > div > div > h3 > a"
+      ).href;
+      return link;
+    });
+    await page.goto(loginPage, {
+      waitUntil: "networkidle2"
+    });
+
+    await page.evaluate(() => {
+      let email = document.querySelector("#email");
+      email.value = "conor.bailey@santandergcb.com";
+      let password = document.querySelector(".password");
+      password.value = "Zildjian01";
+      let link = document.querySelector(
+        "#card-sign-in > form > div > div:nth-child(7) > div > button"
+      );
+      link.click();
+    });
+
+    await page.waitForSelector("#chkAM");
+    await page.waitForSelector("#txtName");
+    await page.waitForSelector("#btn10012");
+
+    await page.evaluate(company => {
+      document.querySelector("#chkAM").click();
+      document.querySelector("#txtName").value = company;
+      document.querySelector("#btn10012").click();
+    }, company);
+
+    await page.waitForNavigation();
+
+    await page.evaluate(dayOfAccess => {
+      let dateDiv = document.createElement("div");
+      dateDiv.style.position = "absolute";
+      dateDiv.style.top = 0;
+      dateDiv.style.left = "50%";
+      dateDiv.style.width = "50%";
+      dateDiv.style.height = "3rem";
+      dateDiv.style.color = "white";
+      dateDiv.style.background = "black";
+      dateDiv.style.zIndex = 9999;
+      dateDiv.style.padding = "5px";
+      document.body.appendChild(dateDiv);
+      dateDiv.innerHTML = `<p>Date: ${dayOfAccess}</p> <p>Address: ${location.href}</p>`;
+    }, dayOfAccess);
+
+    await page.waitForFunction(
+      "document.querySelector('#loading1').style.display == 'none'"
+    );
+
+    await page.screenshot({
+      path: "./public/companyss/dowjones.png"
+      // fullPage: true
+    });
+    console.log(links);
+
+    await browser.close();
+    res.json({
+      links
+    });
+  } catch (e) {
+    console.log(e);
+    res.json({
+      links: "Something went wrong. Please try again."
+    });
+  }
+});
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "companyss.html"));
+});
+
+module.exports = router;
