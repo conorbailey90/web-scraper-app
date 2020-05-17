978873c4aa9be566271602583c268c5f20bc6272
Conor Bailey <conorbailey@conors-mbp.home>
2020-05-17 15:13:49 +0100

    CSS updated

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,11 @@
 const menuToggle = document.querySelector(".menu-toggle");
-const navigation = document.querySelector(".nav-links");
-const dateTime = document.querySelector(".time");
+const navigation = document.querySelector(".mobile-navlinks");
+const menuClose = document.querySelector(".close");
 
 menuToggle.addEventListener("click", () => {
-  navigation.classList.toggle("active");
-  menuToggle.classList.toggle("active");
+  navigation.classList.add("active");
 });
 
-function addZero(n) {
-  return n < 10 ? `0${n}` : n;
-}
-function getTime() {
-  const time = new Date();
-  dateTime.innerHTML = `${addZero(time.getDate())} / ${addZero(
-    time.getMonth() + 1
-  )} / ${time.getFullYear()}</br>${addZero(time.getHours())}:${addZero(
-    time.getMinutes()
-  )}:${addZero(time.getSeconds())}`;
-
-  setTimeout(getTime, 1000);
-}
-
-getTime();
+menuClose.addEventListener("click", () => {
+  navigation.classList.remove("active");
+});
